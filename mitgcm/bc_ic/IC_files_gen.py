17a3a1e9dda2ef0e17ecd8f9bc024049edfcbbb9
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2024-10-02 17:23:05 +0200

    refactoring
    minimal changes to modularise the business logic and make it usable by
    third parties

--- mitgcm/bc_ic/IC_files_gen.py
+++ mitgcm/bc_ic/IC_files_gen.py
@@ -1,4 +1,17 @@
 import argparse
+import os
+
+import numpy as np
+import scipy.io as NC
+from bitsea.commons import netcdf4
+from bitsea.commons.dataextractor import DataExtractor
+from bitsea.commons.mask import Mask
+from bitsea.commons.utils import addsep
+from bitsea.commons.utils import file2stringlist
+from bitsea.general import mask
+from bitsea.general import NetCDF_phys_Files
+from bitsea.general import NetCDF_phys_Vars
+from bitsea.general import space_intepolator_griddata
 
 
 def argument():
@@ -54,32 +67,7 @@ def argument():
     return parser.parse_args()
 
 
-from general import (
-    mask,
-    space_intepolator_griddata,
-    NetCDF_phys_Files,
-    NetCDF_phys_Vars,
-)
-from commons.utils import addsep, file2stringlist
-import scipy.io as NC
-from commons import netcdf4
-from commons.mask import Mask
-import os
-import numpy as np
-from commons.dataextractor import DataExtractor
-
-try:
-    from mpi4py import MPI
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    nranks = comm.size
-except:
-    rank = 0
-    nranks = 1
-
-
-def writeCheckFile():
+def writeCheckFile(M, OUTPUTDIR, var, Mask2):
     Mcheck = M.copy()
     # Mcheck[~Mask2.tmask] = 1.0e+20
 
@@ -94,53 +82,60 @@ def writeCheckFile():
     NCout.close()
 
 
-args = argument()
+def main(
+    inputdir, outputdir, nativemask, outmaskfile, modelvarlist, time, **kwargs
+):
+    INPUTDIR = addsep(inputdir)
+    OUTPUTDIR = addsep(outputdir)
+    os.system("mkdir -p " + OUTPUTDIR)
+    os.system("mkdir -p " + OUTPUTDIR + "CHECK")
+    TIMELIST = file2stringlist(time)
+    Mask_bitsea1 = Mask(nativemask)
+    Mask1 = mask(nativemask)
+    Mask2 = mask(outmaskfile)
 
-INPUTDIR = addsep(args.inputdir)
-OUTPUTDIR = addsep(args.outputdir)
-os.system("mkdir -p " + OUTPUTDIR)
-os.system("mkdir -p " + OUTPUTDIR + "CHECK")
-TIMELIST = file2stringlist(args.time)
-Mask_bitsea1 = Mask(args.nativemask)
-Mask1 = mask(args.nativemask)
-Mask2 = mask(args.outmaskfile)
+    if modelvarlist:
+        MODELVARS = file2stringlist(modelvarlist)
+
+        for var in MODELVARS:
+            inputfile = INPUTDIR + "ave." + TIMELIST[0] + "." + var + ".nc"
+            outBinaryFile = OUTPUTDIR + "IC_" + var + ".dat"
+            B = DataExtractor(Mask_bitsea1, inputfile, var).values
+
+            M = space_intepolator_griddata(Mask2, Mask1, B)
+
+            writeCheckFile(M, OUTPUTDIR, var, Mask2)
+            F = open(outBinaryFile, "wb")
+            for jk in range(Mask2.jpk):
+                F.write(M[jk, :, :])
+            F.close()
+
+    else:
+        MODELVARS = ["U", "V", "T", "S"]
+        for var in MODELVARS:
+            inputfile = (
+                INPUTDIR
+                + TIMELIST[0][:8]
+                + "_"
+                + NetCDF_phys_Files[var]
+                + ".nc"
+            )
+            outBinaryFile = OUTPUTDIR + "IC_" + var + ".dat"
+
+            B = netcdf4.readfile(inputfile, NetCDF_phys_Vars[var])[
+                0, : Mask1.jpk, :, :
+            ]
+            B[~Mask1.tmask] = np.NaN
+            # B[B>1.e+19]  = np.NaN
+
+            M = space_intepolator_griddata(Mask2, Mask1, B)
+            writeCheckFile(M, OUTPUTDIR, var, Mask2)
+            F = open(outBinaryFile, "wb")
+            for jk in range(Mask2.jpk):
+                F.write(M[jk, :, :])
+            F.close()
 
 
-if args.modelvarlist:
-    MODELVARS = file2stringlist(args.modelvarlist)
-
-    for var in MODELVARS[rank::nranks]:
-        inputfile = INPUTDIR + "ave." + TIMELIST[0] + "." + var + ".nc"
-        outBinaryFile = OUTPUTDIR + "IC_" + var + ".dat"
-        print("rank %d working on %s" % (rank, outBinaryFile), flush=True)
-        B = DataExtractor(Mask_bitsea1, inputfile, var).values
-
-        M = space_intepolator_griddata(Mask2, Mask1, B)
-
-        writeCheckFile()
-        F = open(outBinaryFile, "wb")
-        for jk in range(Mask2.jpk):
-            F.write(M[jk, :, :])
-        F.close()
-
-else:
-    MODELVARS = ["U", "V", "T", "S"]
-    for var in MODELVARS[rank::nranks]:
-        inputfile = (
-            INPUTDIR + TIMELIST[0][:8] + "_" + NetCDF_phys_Files[var] + ".nc"
-        )
-        outBinaryFile = OUTPUTDIR + "IC_" + var + ".dat"
-        print("rank %d working on %s" % (rank, outBinaryFile), flush=True)
-
-        B = netcdf4.readfile(inputfile, NetCDF_phys_Vars[var])[
-            0, : Mask1.jpk, :, :
-        ]
-        B[~Mask1.tmask] = np.NaN
-        # B[B>1.e+19]  = np.NaN
-
-        M = space_intepolator_griddata(Mask2, Mask1, B)
-        writeCheckFile()
-        F = open(outBinaryFile, "wb")
-        for jk in range(Mask2.jpk):
-            F.write(M[jk, :, :])
-        F.close()
+if __name__ == "__main__":
+    args = argument()
+    exit(main(**vars(args)))
